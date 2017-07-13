ff366d442ed1e75139efcc116222a118b1e360bf
Sean Connelly <sconnel42@gmail.com>
2017-07-13 11:05:40 -0500

    Changed landing page design

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os, signal
-from flask import Flask
+from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
@@ -8,10 +8,8 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1 style="color: red;">'
-    page += generator.generate_buzz()
-    page += '</h1></body></html>'
-    return page
+    buzz = generator.generate_buzz()
+    return render_template('index.html', buzz=buzz)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.getenv('PORT')) # port 5000 is the default
