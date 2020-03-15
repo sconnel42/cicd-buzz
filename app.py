fdac91f75532f53d1433ad1bda11a869d41823cc
Sean Connelly <sconnel42@gmail.com>
2020-03-15 16:23:33 -0500

    Linting and making generator work with python3

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-import os, signal
+import os
+import signal
+
 from flask import Flask
 from buzz import generator
 
 app = Flask(__name__)
 
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+
 
 @app.route("/")
 def generate_buzz():
@@ -13,5 +16,6 @@ def generate_buzz():
     page += '</h1></body></html>'
     return page
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv('PORT')) # port 5000 is the default
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))  # port 5000 is the default
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,19 +1,20 @@
-import unittest
-
 from buzz import generator
 
+
 def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
-    word = generator.sample(l)
-    assert word in l
+    word_list = ('foo', 'bar', 'foobar')
+    word = generator.sample(word_list)
+    assert word in word_list
+
 
 def test_sample_multiple_words():
-    l = ('foo', 'bar', 'foobar')
-    words = generator.sample(l, 2)
+    word_list = ('foo', 'bar', 'foobar')
+    words = generator.sample(word_list, 2)
     assert len(words) == 2
-    assert words[0] in l
-    assert words[1] in l
+    assert words[0] in word_list
+    assert words[1] in word_list
     assert words[0] is not words[1]
+
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
